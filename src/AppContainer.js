f37328826559730939eda890e350a406af4e5397
alexandermagee <alexandermagee1@gmail.com>
2020-05-09 15:47:57 +0100

    Setting up initial file structure

---
+++ src/AppContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class AppContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+    render(){
+    return (
+        <h1>Weather App Container </h1>
+    )
+    }
+
+}
